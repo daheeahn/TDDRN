8479dd3fbaa0c855324be22bbcb1de9e817afec4
daheeahn <deg9810@likelion.org>
2019-09-19 10:35:20 +0900

    ToDoItem Component - complete 버튼 누를 때 구현 완료

--- ToDoItem.js
+++ ToDoItem.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
 import { View, Text, Button, StyleSheet } from 'react-native'
 export default class ToDoItem extends Component {
+    _onCompleted = () => {
+        const { onCompleted, index } = this.props;
+        onCompleted(index);
+    }
+
     render() {
         const { item } = this.props;
         return (
             <View style={item.completed ? styles.completed : styles.default}>
                 <Text></Text>
-                <Button title="C" />
+                <Button title="C" onPress={this._onCompleted} />
                 <Button title="D" />
             </View>
         )
